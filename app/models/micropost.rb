1ed4368d2bb8291182ec8d67617d29576de02cf3
tatsuya <programming00lesson@gmail.com>
2020-11-21 06:01:43 +0000

    favouser delete

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,8 +3,7 @@ class Micropost < ApplicationRecord
   
   validates :content, presence: true, length: { maximum: 255 }
   
-  has_many :reverses_of_favorite, class_name: "Favorite", foreign_key: "favomicropost_id"
-  has_many :favousers, through: :favorites, source: :user
+
   
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
       get :followings
       get :followers
       get :likes
-      get :favousers
     end
   end
 
