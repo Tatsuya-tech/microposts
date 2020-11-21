3524bac22645002ddd59480040a152f9abdd72b1
tatsuya <programming00lesson@gmail.com>
2020-11-21 09:14:20 +0000

    navbar-favorites-add

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,13 +5,13 @@ before_action :require_user_logged_in
     micropost = Micropost.find(params[:favomicropost_id])
     current_user.favomicropost(micropost)
     flash[:success] = '投稿をお気に入りしました。'
-    redirect_to users_path
+    redirect_to user_path(current_user)
   end
 
   def destroy
     micropost = Micropost.find(params[:favomicropost_id])
     current_user.unfavomicropost(micropost)
     flash[:success] = '投稿のお気に入りを解除しました。'
-    redirect_to users_path
+    redirect_to user_path(current_user)
   end
 end
